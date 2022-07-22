b5a2ec8cae53442badbf5df83cf3a8400e9967c6
taufikrafli138 <Taufikcahyo45@gmail.com>
2022-07-22 15:48:34 +0700

    fix

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -25,7 +25,7 @@ $menu_layanan  = $menu->layanan();
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<<<<<<< HEAD
+
   <title>SCTC - Living Lab</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
@@ -33,7 +33,7 @@ $menu_layanan  = $menu->layanan();
   <!-- Favicons -->
   <link href="<?php echo base_url() ?>/assets/upload/image/SCTC/Logo.png" rel="icon">
   <link href="<?php echo base_url() ?>/assets/dasbor/img/apple-touch-icon.png" rel="apple-touch-icon">
-=======
+
 <title>Dewi Bootstrap Template - Index</title>
 <meta content="" name="description">
 <meta content="" name="keywords">
@@ -41,7 +41,7 @@ $menu_layanan  = $menu->layanan();
 <!-- Favicons -->
 <link href="<?php echo base_url() ?>/assets/dasbor/img/favicon.png" rel="icon">
 <link href="<?php echo base_url() ?>/assets/dasbor/img/apple-touch-icon.png" rel="apple-touch-icon">
->>>>>>> 7474546edc4a794d1b90a8f0c9ea2863570db5cc
+
 
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -65,58 +65,18 @@ $menu_layanan  = $menu->layanan();
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 
-<<<<<<< HEAD
+
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-        <div class="container d-flex align-items-center justify-content-between">
-
-      <h1 class="logo"><img src="<?php echo base_url() ?>/assets/upload/image/SCTC/Logo.png"><a href="index.html" style="margin-left:10px;">SCTC</a></h1>
-      
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-    </div>
-  </header><!-- End Header -->
 
 
 
-=======
+
 <!-- ======= Header ======= -->
 <header id="header" class="fixed-top">
   <div class="container d-flex align-items-center justify-content-between">
->>>>>>> 7474546edc4a794d1b90a8f0c9ea2863570db5cc
 
-    <h1 class="logo"><a href="index.html">SCTC</a></h1>
+
+    <h1 class="logo"><img src="<?php echo base_url() ?>/assets/upload/image/SCTC/Logo.png"><a href="index.html" style="margin-left:10px;">SCTC</a></h1>
 
     <!-- Uncomment below if you prefer to use an image logo -->
     <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->

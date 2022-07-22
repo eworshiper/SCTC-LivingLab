f73477064fca61d0bbb3049358ef6b68ba554ffb
eworshiper <purba.evenetus2@gmail.com>
2022-07-22 13:14:39 +0700

    no message

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -11,7 +11,7 @@ $layanan      = $menu->layanan();
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
-      <h1>Plan. Launch. Grow.</h1>
+      <h1>SCTC Living Lab</h1>
       <h2>We are team of talented designers making websites with Bootstrap</h2>
       <div class="d-flex">
         <a href="#about" class="btn-get-started scrollto">Get Started</a>

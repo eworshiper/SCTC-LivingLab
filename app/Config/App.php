a153db3bebde04d4ed613aabfc0d3ddfab40e63d
taufikrafli138 <Taufikcahyo45@gmail.com>
2022-07-22 11:27:30 +0700

    no message

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/SCTC/';
+	public $baseURL = 'http://localhost/SCTC-Livinglab/';
 
 	/**
 	 * --------------------------------------------------------------------------
